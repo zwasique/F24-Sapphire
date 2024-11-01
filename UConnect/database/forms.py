0ffb0cb9d30101ceb33f76ce2aa0da787c599663
Anna Foust <annagfoust@gmail.com>
2024-11-01 19:03:57 -0400

    beginning code for user profile

---
+++ UConnect/database/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+# class for making a user's profile; the forms should get the user's first and last name as well as biography
+class ProfileForm(forms.Form):
+    school_email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    first_name = forms.CharField(label="First name", max_length=50)
+    last_name = forms.CharField(label="Last name", max_length=50)
+    biography = forms.CharField(label="Biography", widget=forms.Textarea)
--- UConnect/database/views.py
+++ UConnect/database/views.py
@@ -1,4 +1,38 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
+from .forms import ProfileForm
+from .models import User
 
 def home(request):
     return render(request, 'database/home.html', {})
+
+def profile_form(request):
+    if request.method == "POST":
+        first_name = ProfileForm.first_name(request.POST)
+        last_name = ProfileForm.last_name(request.POST)
+        user_biography = ProfileForm.biography(request.POST)
+
+        if form.is_valid():
+            User.first_name = first_name
+            User.last_name = last_name
+            User.biography = user_biography
+            return HttpResponseRedirect("/profile/") #will redirect to the profile page if form is valid
+        
+    else:
+        form = ProfileForm()
+    
+    return render(request, "database/profilecreation.html", {"form": form})
+
+def profile(request):
+    first_name = User.objects.get(pk=3)
+    last_name = User.objects.get(pk=4)
+    biography = User.objects.get(pk=5)
+
+    context={
+        'first_name':first_name,
+        'last_name':last_name,
+        'biography':biography
+    }
+
+    return render(request, "database/profile.html", {'profile': context})    
