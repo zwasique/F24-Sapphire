fd4203a396c9c4b0d966c89fefa19317fbd98694
Jasmine Willis <jwill071@odu.edu>
2024-11-17 21:44:20 -0500

    removed project_tags widget from forms.py

--- UConnect/database/forms.py
+++ UConnect/database/forms.py
@@ -10,24 +10,7 @@ class CreatePost(ModelForm):
         label="Desired Number of Seekers: ", min_value=1, max_value=15, help_text="No more than 15")
     project_length = forms.IntegerField(
         label="Expected Project Length", min_value=0, max_value=24, help_text="Estimated length of project in months")
-
-    #initial tag edits; editing later
-    PROJECT_TAG_CHOICES = ["undergraduate", "graduate", "research", "laboratory", "survey", "experiment", "academic", "personal", "interview", "accounting", "linguistics", "ESL", "TESL",
-    "art_history", "visual_fine_art", "biochemistry", "microbiology", "prehealth", "marine_biology", "chemistry", "business_admin", "economics", "finance", "marketing", "engineering", 
-    "communication", "cinema", "media_studies", "mathematics", "data_science", "simulation", "compsci", "info_technology", "counseling", "writing", "criminology", "cybersecurity", "AI", 
-    "dental_hygiene", "ECE", "ecology", "aerospace", "biomedical", "mechanical", "english", "lit_and_comp", "journalism", "fine_arts", "graphic_design", "painting", "drawing", "photography", 
-    "print_media", "geography", "urban_planning", "health_science", "history", "human_services", "app development", "database", "ecommerce", "infosys", "game_studies", "game_design", 
-    "leadership", "intl studies", "big_data", "analytics", "lab_science", "ROTC", "music", "vocal", "music recording", "naval_science", "nursing", "midwifery", "postlicensure", "prelicensure", 
-    "earth_science", "oceanography", "philosophy", "politics", "legal_studies", "religion", "phys_ed", "coaching", "phys_therapy", "physics", "astrophysics", "polisci", "global_politics", 
-    "psychology", "public_admin", "public_health", "reading", "sociology", "audiology", "theater", "theatre", "dance", "design_tech", "performance", "womens_studies", "world_languages", 
-    "world_cultures", "french", "german", "japanese", "spanish", "arabic", "programming", "ethics", "digital_art", "design", "modeling", "biology"] 
-
-    project_tags = forms.MultipleChoiceField(
-        required=True,
-        widget=forms.CheckboxSelectMultiple,
-        choices=PROJECT_TAG_CHOICES,
-    )
-    
+    #project_tags
     post_body = forms.CharField(
         label="Project Description: ", max_length=4000, help_text="Visit the Home Page if you need an example",
         widget=forms.Textarea)
