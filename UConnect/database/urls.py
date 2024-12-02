bc61b5048f5b22c656ceb5a6bdd41a257506bffc
Aria A. <ariaprofessionalcontact@gmail.com>
2024-12-02 01:57:28 -0500

    Revert "removed db.sqlite3"
    This reverts commit 2f5b1add463c92415e11db52ca452c6cd97332e6, reversing
    changes made to 7aa50f393a3439a22af95f2a7f2cc4c9eadd2342.

--- UConnect/database/urls.py
+++ UConnect/database/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-
-from . import views
-
+from . import views 
 urlpatterns = [
-    path('', views.login, name="login"),
     path('home/', views.home, name="home"),
-    path('signup/', views.profile_form, name="profile_form"),
+    path('profilecreation/', views.profile_form, name="profile_form"),
     path('profile/', views.profile, name="profile"),
     path('postcreation/', views.create_post, name="create_post"),
     path('posts/', views.posts, name="posts"),
@@ -29,5 +26,4 @@ urlpatterns = [
     path('inbox/', views.inbox, name="inbox"),
     path('search/', views.search, name="search"),
     path('account/', views.account , name="account"),
-    path('login/', views.login, name="login"),
 ]
--- UConnect/database/views.py
+++ UConnect/database/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from .forms import PostForm, ProfileForm
-from .models import User, UserPost
-
+from .forms import ProfileForm
+from .forms import CreatePost
+from .models import User
+from .models import UserPost
 
 def login(request):
     return render(request, 'database/pages/login.html', {})
@@ -13,6 +14,9 @@ def home(request):
 
 def posts(request):
     return render(request, 'database/pages/posts.html')
+
+def launch(request):
+    return render(request, 'database/pages/launch.html')
 
 def account(request):
     return render(request, 'database/pages/account.html')
@@ -26,27 +30,12 @@ def profile_form(request):
 
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect("../home") # Note: Use URLs based on urls.py rather than directly referencing html files
+            return HttpResponseRedirect("database/pages/home.html") #will redirect to the home page if form is valid, however, this doesn't work right now
         
     else:
         form = ProfileForm()
     
     return render(request, "database/pages/signup.html", {"form": form})
-
-
-def launch(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("../home")
-
-    else:
-        form = PostForm()
-
-    return render(request, 'database/pages/launch.html', {"form": form})
-
 
 def profile(request):
     # ChatGPT code start
@@ -63,14 +52,14 @@ def profile(request):
 
 def create_post(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = CreatePost(request.POST)
 
         if form.is_valid():
             form.save()
             return HttpResponseRedirect("/profile/") # eventually want this to be search page
 
     else:
-        form = PostForm()
+        form = CreatePost()
 
     return render(request, "database/pages/searching.html", {"form": form})
 
