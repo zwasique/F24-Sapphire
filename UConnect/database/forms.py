eeac039a9260252ac37b77bf5ac071360517470d
Anna Foust <annagfoust@gmail.com>
2024-12-02 19:16:13 -0500

    brought back deleted lines that should not have been deleted

--- UConnect/database/forms.py
+++ UConnect/database/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import ModelForm
 
 from .models import User
-
+from .models import Tag
 
 class CreatePost(ModelForm):
     project_name = forms.CharField(label="Project Name: ", max_length=50, help_text="Focus on what the project does")
@@ -22,6 +22,8 @@ class ProfileForm(ModelForm):
     first_name = forms.CharField(label="First name", max_length=50)
     last_name = forms.CharField(label="Last name", max_length=50)
     biography = forms.CharField(label="Biography", widget=forms.Textarea)
+    tags = forms.ModelMultipleChoiceField(label="Select up to 6 tags.", queryset=Tag.objects.all(), widget=forms.CheckboxSelectMultiple)
+    #to do: implement tag limit
     class Meta:
         model = User
         fields = ['school_email', 'password', 'first_name', 'last_name', 'biography', 'tags']
