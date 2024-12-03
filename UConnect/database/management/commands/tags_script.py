9d1cf09d91575d7c01243dd90f7247d3af06a407
Anna Foust <annagfoust@gmail.com>
2024-12-02 19:05:13 -0500

    brought the tags script back

---
+++ UConnect/database/management/commands/tags_script.py
@@ -0,0 +1,17 @@
+import csv
+
+from database.models import Tag
+from django.core.management.base import BaseCommand
+
+
+# to use: in the F24-Sapphire/UConnect folder, run 'python manage.py tags_script'
+class Command(BaseCommand):
+    help = "Adds tags to the database."
+
+    def handle(self, *args, **options):
+        with open("database/tags.csv") as file:
+            reader = csv.reader(file)
+            for entry in reader:
+                Tag.objects.get_or_create(
+                    name = entry[0]
+                )
