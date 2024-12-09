ceaacb488844daf9bc041d6aaefba4dcd5e01f25
Jasmine Willis <jwill071@odu.edu>
2024-12-09 11:53:43 -0500

    going step by step added login url

--- UConnect/database/urls.py
+++ UConnect/database/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('inbox/', views.inbox, name="inbox"),
     path('search/', views.search, name="search"),
     path('account/', views.account , name="account"),
-    path('', views.login, name="login"),
+    path('login/', views.login, name="login"),
 ]
